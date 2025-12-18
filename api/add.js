efa71f9091cad190f83846b36cda06d4601f9bcf
aldlemeanwar23-del <aldlemeanwar23@gmail.com>
2025-12-18 09:02:22 +0000

    Update add.js

--- api/add.js
+++ api/add.js
@@ -5,11 +5,11 @@ export default async function handler(req, res) {
   if (req.method !== 'POST')
     return res.status(405).json({ message:'Method not allowed' });
 
-  const { device_id,pname,pmobile,email,age } = req.body;
+  const { device_id,pname,pmobile,email,age,chin,chout } = req.body;
 
   await sql`
-    INSERT INTO informations(device_id,p_name,ph_no,email,age)
-    VALUES(${device_id},${pname},${pmobile},${email},${age})
+    INSERT INTO informations(device_id,p_name,ph_no,email,age,checkin_date,checkout_date)
+    VALUES(${device_id},${pname},${pmobile},${email},${age},${checkin_date},${checkout_date})
   `;
 
   res.json({ success:true });
