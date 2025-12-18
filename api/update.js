a32f36c3837a9b9de65e06dcc0f657fc2887b2f5
aldlemeanwar23-del <aldlemeanwar23@gmail.com>
2025-12-18 09:08:15 +0000

    Update update.js

--- api/update.js
+++ api/update.js
@@ -2,10 +2,10 @@ import { neon } from "@neondatabase/serverless";
 const sql = neon(process.env.DATABASE_URL);
 
 export default async function handler(req,res){
-  const { device_id,pname,pmobile,email,age } = req.body;
+  const { device_id,pname,pmobile,email,age,chin,chout } = req.body;
   await sql`
     UPDATE informations
-    SET p_name=${pname}, ph_no=${pmobile}, email=${email}, age=${age}
+    SET p_name=${pname}, ph_no=${pmobile}, email=${email}, age=${age},checkin_date=${checkin_date},checkout_date=${checkout_date}
     WHERE device_id=${device_id}
   `;
   res.json({ success:true });
