6e868d25d3231c18ed3710f0d6736c4667177bc1
aldlemeanwar23-del <aldlemeanwar23@gmail.com>
2025-12-18 07:03:14 +0000

    Delete api/import { neon } from "@neondatabase/infoDelete.js

--- api/import { neon } from "@neondatabase/infoDelete.js
+++
@@ -1,8 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-const sql = neon(process.env.DATABASE_URL);
-
-export default async function handler(req,res){
-  const { id } = req.body;
-  await sql`DELETE FROM informations WHERE id=${id}`;
-  res.json({ success:true });
-}
