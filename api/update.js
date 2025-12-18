8b73fa1f1150959da1e00fe10b1a92ee3f978aa1
aldlemeanwar23-del <aldlemeanwar23@gmail.com>
2025-12-18 09:46:49 +0000

    Update update.js

--- api/update.js
+++ api/update.js
@@ -5,7 +5,7 @@ export default async function handler(req,res){
   const { device_id,pname,pmobile,email,age,chin,chout } = req.body;
   await sql`
     UPDATE informations
-    SET p_name=${pname}, ph_no=${pmobile}, email=${email}, age=${age},checkin_date=${checkin_date},checkout_date=${checkout_date}
+    SET p_name=${pname}, ph_no=${pmobile}, email=${email}, age=${age},checkin_date=${chin},checkout_date=${chout}
     WHERE device_id=${device_id}
   `;
   res.json({ success:true });
