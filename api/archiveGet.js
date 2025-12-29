558d4397dfc88740529b8c67c70e35d419d77a96
aldlemeanwar23-del <aldlemeanwar23@gmail.com>
2025-12-29 12:11:26 +0300

    Update archiveGet.js

--- api/archiveGet.js
+++ api/archiveGet.js
@@ -1,21 +1,27 @@
-// pages/api/archiveGet.js
 import { neon } from "@neondatabase/serverless";
-
 const sql = neon(process.env.DATABASE_URL);
 
 export default async function handler(req, res) {
   try {
-    const { search } = req.query; // قراءة باراميتر البحث من URL
+    const { search } = req.query;
 
     let data;
     if (search) {
-      // إذا تم تمرير اسم المريض، البحث عنه
       data = await sql`
-       SELECT * FROM sensor_data_archive
-        WHERE p_name LIKE ${'%' + search + '%'}
-        ORDER BY timestamp DESC
+        SELECT s.id, s.device_id, s.heartrate, s.spo2, s.timestamp, i.p_name
+        FROM sensor_data_archive s
+        LEFT JOIN informations i ON i.device_id = s.device_id
+        WHERE i.p_name ILIKE ${'%' + search + '%'}
+        ORDER BY s.timestamp DESC
       `;
-    } 
+    } else {
+      data = await sql`
+        SELECT s.id, s.device_id, s.heartrate, s.spo2, s.timestamp, i.p_name
+        FROM sensor_data_archive s
+        LEFT JOIN informations i ON i.device_id = s.device_id
+        ORDER BY s.timestamp DESC
+      `;
+    }
 
     res.status(200).json(data);
   } catch (err) {
