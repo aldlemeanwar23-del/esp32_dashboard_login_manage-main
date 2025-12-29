d29d571cb6c8607cc9c734bb5753f8d4a11fc75d
aldlemeanwar23-del <aldlemeanwar23@gmail.com>
2025-12-29 12:12:26 +0300

    Update archiveGet.js

--- api/archiveGet.js
+++ api/archiveGet.js
@@ -14,13 +14,6 @@ export default async function handler(req, res) {
         WHERE i.p_name ILIKE ${'%' + search + '%'}
         ORDER BY s.timestamp DESC
       `;
-    } else {
-      data = await sql`
-        SELECT s.id, s.device_id, s.heartrate, s.spo2, s.timestamp, i.p_name
-        FROM sensor_data_archive s
-        LEFT JOIN informations i ON i.device_id = s.device_id
-        ORDER BY s.timestamp DESC
-      `;
     }
 
     res.status(200).json(data);
